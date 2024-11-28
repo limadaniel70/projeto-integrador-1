d96fc19ff2338edeaa718f071700b4fe4871ba3f
Daniel Lima <dnlima77237@gmail.com>
2024-11-28 09:56:41 -0300

    fix

--- script.py
+++ script.py
@@ -26,6 +26,12 @@ def devolver_livro(nome):
 def adicionar_livro(nome):
     livros[nome] = "disponivel"
 
+def remover_livro(nome):
+    if livros.get(nome) is None:
+        print("Livro inexistente!")
+        return
+    del livros[nome]
+    print("Livro removido com sucesso!")
 
 def listar_livros():
     for livro, disponibilidade in livros.items():
@@ -40,13 +46,15 @@ print(
 1. Listar livros.
 2. Pegar livro.
 3. Devolver livro.
-4. Adicionar livro"""
+4. Adicionar livro.
+5. Deletar livro."""
 )
 
 while True:
     print("-" * 40)
 
-    ans = input("Insira sua resposta: ")
+    print("Insira uma opção")
+    ans = input("> ")
 
     match ans:
         case "1":
@@ -61,5 +69,8 @@ while True:
             livro = input("Insira o nome do livro: ")
             adicionar_livro(livro)
             print("Livro inserido com sucesso!")
+        case "5":
+            livro = input("Insira o nome do livro: ")
+            remover_livro(livro)
         case _:
             print("Insira um número valido!")
